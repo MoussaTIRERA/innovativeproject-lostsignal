a2ec273c8a5209d9dea7880acbae7ae21e5fdac0
Aleksander Boruch-Gruszecki <aleksander.boruch-gruszecki@bright-it.com>
2014-11-23 22:26:03 +0100

    Add port detecting for heroku

--- server/lostsignal.js
+++ server/lostsignal.js
@@ -1,6 +1,8 @@
 var http = require('http');
 var fs = require('fs');
 var pg = require('pg');
+var app = require('express')();
+var bodyParser = require('body-parser');
 var conString = "postgres://aleksbg@localhost/lostsignal";
 
 function queryobj(lon, lat) { return {
@@ -37,53 +39,61 @@ function insertJson(json) {
 function log(msg) { return console.log(msg); }
 function error(msg) { return console.error(msg); }
 
-var server = http.createServer( function(req, res) {
-    console.dir(req.param);
+app.use(bodyParser.json());
 
-    if (req.method == 'POST') {
-        log('POST');
-        var body = '';
-        req.on('data', function (data) {
-            body += data;
-            log('Partial body: ' + body);
-        });
-        req.on('end', function () {
-            log('Body: ' + body);
-            var json;
-            try {
-                json = JSON.parse(body);
-		insertJson(json);
-            } catch (err) {
-                if (err.name === 'SyntaxError') {
-                    return log('not a valid json');
-                } else {
-                    res.writeHead(200, {'content-type': 'text/plain'});
-                    res.end('post received\n');
-                    throw err;	// welp
-                }
+app.post('/', function(req, res) {
+    log('post');
+    var body = '';
+    req.on('data', function (data) {
+        body += data;
+        log('partial body: ' + body);
+    });
+    req.on('end', function () {
+        log('body: ' + body);
+        var json;
+        try {
+            json = JSON.parse(body);
+            insertJson(json);
+        } catch (err) {
+            if (err.name === 'SyntaxError') {
+                return log('not a valid json');
+            } else {
+                res.send('post received\n');
+                throw err;	// welp
             }
-            res.writeHead(200, {'content-type': 'text/plain'});
-            res.end('post received\n');
-        });
-    } else if (req.method === 'GET') {
-        log('GET');
-        pg.connect(conString, function(err, client, done) {
-            if (err) {
-                return error('error: ', err);
-            }
-            client.query(queryobj(17.031853, 51.110124), function(err, result) {
-                //call `done()` to release the client back to the pool
-                done();
-
-                if(err) {
-                    return error('error running query', err);
-                }
-                res.writeHead(200, {'content-type': 'application/json'});
-                res.end(JSON.stringify(result.rows));
-                log('sent rows');
-            });
-        });
-    }
+        }
+        res.send('post received\n');
+    });
 });
 
-server.listen(9501);
+app.get('/', function(req, res) {
+    log('get');
+    pg.connect(conString, function(err, client, done) {
+        if (err) {
+            return error('error: ', err);
+        }
+        client.query(queryobj(17.031853, 51.110124), function(err, result) {
+            //call `done()` to release the client back to the pool
+            done();
+
+            if(err) {
+                return error('error running query', err);
+            }
+            res.send(JSON.stringify(result.rows));
+            log('sent rows');
+        });
+    });
+});
+
+log(typeof process.env.PORT);
+log(process.env.PORT);
+var port;
+if (process.env.PORT == null) {
+    port = 9501;
+} else {
+    port = parseInt(process.env.PORT);
+}
+log(port);
+app.listen(port, function() {
+    log("up!");
+});
