b931edf1ca07888dfcbb2a4debaae2285cb33998
Aleksander Boruch-Gruszecki <aleksander.boruch-gruszecki@bright-it.com>
2014-11-24 17:15:11 +0100

    Add heroku db support
    Use correct name for longitude
    Temporarily use separate queries for different dbs

--- server/lostsignal.js
+++ server/lostsignal.js
@@ -4,8 +4,10 @@ var pg = require('pg');
 var app = require('express')();
 var bodyParser = require('body-parser');
 var conString = "postgres://aleksbg@localhost/lostsignal";
+var conHeroku = process.env.DATABASE_URL;
+var herokuDb = conHeroku != null ? true : false;
 
-function queryobj(lon, lat) { return {
+var queryobj = function (lon, lat) { return {
     name: 'select-by-distance',
     text: 'select provider, strength, ST_X(location::geometry) as long,' +
         'ST_Y(location::geometry) as lat from lostsignal WHERE ' +
@@ -13,25 +15,38 @@ function queryobj(lon, lat) { return {
     values: [lon, lat]
 }}
 
-function queryobj2(lon, lat, provider, str, uuid) { return {
+var queryobj2 = function (lon, lat, provider, str, uuid) { return {
     name: 'insert',
     text: 'insert into lostsignal values (ST_Point($1, $2), $3, $4, $5)',
     values: [lon, lat, provider, str, uuid]
 }}
 
+var querytemp = function (lon, lat) { return {
+    name: 'select',
+    text: 'select provider, strength, longitude, latitude from lostsignal limit 10'
+}}
+
+var querytemp2 = function (lon, lat, provider, str, uuid) { return {
+    name: 'insert',
+    text: 'insert into lostsignal values ($1, $2, $3, $4, $5)',
+    values: [lon, lat, provider, str, uuid]
+}}
+
 function insertJson(json) {
-    pg.connect(conString, function(err, client, done) {
+    var con = herokuDb ? conHeroku : conString;
+    var qfun = herokuDb ? querytemp2 : queryobj2;
+    pg.connect(con, function(err, client, done) {
         if (err) {
             return error('error: ', err);
         }
-        client.query(queryobj2(json.longtitude, json.latitude, json.network,
-                    "1", json.uuid), function(err, result) {
+        client.query(qfun(json.longitude, json.latitude, "Plus",
+                    "1", "f93db9a0-7482-11e4-b61e-0002a5d5c51b"), function(err, result) {
             done();
 
             if(err) {
                 return error('error running query', err);
             }
-            log('sent rows');
+            log('inserted rows');
         });
     });
 }
@@ -44,35 +59,37 @@ app.use(bodyParser.json());
 app.post('/', function(req, res) {
     log('post');
     var body = '';
-    req.on('data', function (data) {
-        body += data;
-        log('partial body: ' + body);
-    });
+    req.on('data', function (data) { body += data; });
     req.on('end', function () {
         log('body: ' + body);
         var json;
         try {
             json = JSON.parse(body);
+            log('lon: ' + json.longitude + ' / lat: ' + json.latitude);
             insertJson(json);
         } catch (err) {
             if (err.name === 'SyntaxError') {
-                return log('not a valid json');
+                log('not a valid json');
             } else {
-                res.send('post received\n');
-                throw err;	// welp
+                res.send({});
+                throw err;	// temporary to alert
             }
+        } finally {
+            res.send({});
         }
-        res.send('post received\n');
     });
 });
 
 app.get('/', function(req, res) {
-    log('get');
-    pg.connect(conString, function(err, client, done) {
+    log('get'); log(herokuDb);
+    var con = herokuDb ? conHeroku : conString;
+    var qfun = herokuDb ? querytemp : queryobj;
+    log(qfun().text);
+    pg.connect(con, function(err, client, done) {
         if (err) {
             return error('error: ', err);
         }
-        client.query(queryobj(17.031853, 51.110124), function(err, result) {
+        client.query(qfun(17.031853, 51.110124), function(err, result) {
             //call `done()` to release the client back to the pool
             done();
 
@@ -85,15 +102,14 @@ app.get('/', function(req, res) {
     });
 });
 
-log(typeof process.env.PORT);
-log(process.env.PORT);
 var port;
 if (process.env.PORT == null) {
     port = 9501;
 } else {
     port = parseInt(process.env.PORT);
 }
-log(port);
 app.listen(port, function() {
-    log("up!");
+    log('Found port:' + port);
+    log('On heroku db: ' + herokuDb);
+    log('up!');
 });
