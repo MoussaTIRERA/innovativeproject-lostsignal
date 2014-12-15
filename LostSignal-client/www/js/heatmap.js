038209f8b442509d160554f2da92f25670748563
MGot <marcin.gotszling@gmail.com>
2014-12-15 12:13:25 +0100

    maps

--- LostSignal-client/www/js/heatmap.js
+++ LostSignal-client/www/js/heatmap.js
@@ -27,7 +27,48 @@ function heatmap_initialize() {
         map: hmap,
         data: pointArray
     });
-    setInterval(getHeatInfo(),5000);
+    setInterval(function () {
+        if(navigator.geolocation) {
+         navigator.geolocation.getCurrentPosition(function(position) {
+             var myLatlng = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+
+            //dataPoints.push({location: myLatlng, weight: currentSignal});
+            dataPoints.push(myLatlng);
+            pointArray = new google.maps.MVCArray(dataPoints);
+            heatmap.setData(pointArray);
+
+            var infowindow = new google.maps.InfoWindow({
+               content: '<p style="color:black">Location found using HTML5.</p>' +
+                '<p style="color:black">Actual position:' + myLatlng +'</p>' +
+                '<p style="color:black">Date: ' + new Date() + '</p>'
+                //+'<p style="color:black">Signal strength: ' + currentSignal + '</p>'
+            });
+
+            var marker = new google.maps.Marker({
+                position: myLatlng,
+                map: hmap
+            });
+            google.maps.event.addListener(marker, 'click', function() {
+               infowindow.open(hmap,marker);
+            });
+         hmap.setCenter(myLatlng);
+     }, handleNoGeolocation);
+
+         } else {
+             handleNoGeolocation();
+
+             var onSuccess = function(position) {
+
+             };
+
+             function onError() {
+             alert('Brak zasięgu WiFi, pakietu oraz GPS!');
+             }
+
+             navigator.geolocation.getCurrentPosition(onSuccess, onError);
+         }
+    },5000); // taki interwał chodzi
+    //setInterval(getHeatInfo(),5000); // taki interwał nie chodzi
 }
 
 function heatmap_populate() {
@@ -53,9 +94,9 @@ function setDatas() {
 
 function getHeatInfo() {
 
-    alert("dodano");
-    /*if(navigator.geolocation) {
+    if(navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
+            //alert("dodano");
             var myLatlng = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
 
             //dataPoints.push({location: myLatlng, weight: currentSignal});
@@ -92,5 +133,5 @@ function getHeatInfo() {
         }
 
         navigator.geolocation.getCurrentPosition(onSuccess, onError);
-    }*/
+    }
 }
