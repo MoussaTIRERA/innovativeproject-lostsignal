6b736d2d46bf219bb06f15a117a1206618944bc5
MGot <marcin.gotszling@gmail.com>
2015-01-24 19:38:52 +0100

    working compass

--- LostSignal-client/www/js/navigation.js
+++ LostSignal-client/www/js/navigation.js
@@ -3,26 +3,26 @@ var watchID;
 
 function navigate() {
     var options = {
-        frequency: 2000
+        frequency: 1000
     };
     watchID = navigator.compass.watchHeading(onSuccess1, onError1, options);
 
 }
 
-
-
-
 function onSuccess1(heading) {
     if(bestPosition == lastPostition)
         alert("Jestes u celu");
 
-    //var nav = azimuth(Position.coords.latitude,Position.coords.longitude,bestLat,bestLng);
+    var nav = azimuth(Position.coords.latitude,Position.coords.longitude,bestLat,bestLng);
 
 
-    var direction = Math.round(heading.magneticHeading);
-    alert(direction);
+    var direction = Math.round(360-heading.magneticHeading-nav);
     var angle = document.getElementById('heading');
+    var azim = document.getElementById('azimuth');
+
     angle.innerHTML = 'Heading:' + direction;
+    azim.innerHTML = 'Azimuth:' + Math.round(nav);
+
 
     $("#directionArrow").rotate(direction);
 };
@@ -31,18 +31,34 @@ function onError1(error) {
     alert('CompassError: ' + error.code);
 };
 
-
 function azimuth(x1,y1,x2,y2) {
-    var x = x2-x1;
-    var y = y2-y1;
-    var result = Math.atan2(x,y)*200.0/Math.PI;
-    if(result<0)
-        result +=360.0;
-    return result;
+    var pi = 4.0*Math.atan(1.0);
+    var rs = 180.0/pi;
+    var a,az;
+
+    x = x2-x1;
+    y = y2-y1;
+
+    if(x == 0) {
+        if (y > 0)
+            a = pi / 2;
+        else
+            a = 1.5*pi;
+    }
+    else {
+        a = Math.atan(y/x);
+        if(x < 0)
+            a += pi;
+        else {
+            if(y<0)
+                a += 2*pi;
+        }
+    }
+    if(x != 0)
+        az = a*rs;
+
+    return az;
 }
-
-
-
 
 $(document).on( "click", '#runCompass', function() {
     if(bestPosition == null) {
@@ -53,6 +69,7 @@ $(document).on( "click", '#runCompass', function() {
 
         },1000);
     }
+    alert("Calibrate Your compass then click 'Ok'");
     setTimeout(function(){
         navigate();
     },3000);
